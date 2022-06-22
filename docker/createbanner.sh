1af0a7147eb406ad86232f2e1d1e84204bf64028
Vasilis Naserentin <vasilis.naserentin@chalmers.se>
2022-06-22 14:11:50 +0000

    Removed banner for now.

---
+++ docker/createbanner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Welcome to the Digital Twin Cities Platform" > banner
+echo "-------------------------------------------" >> banner
+echo " " >> banner
+echo "  ██████╗ ████████╗ ██████╗ ██████╗">>banner
+echo "  ██╔══██╗╚══██╔══╝██╔════╝██╔════╝">>banner
+echo "  ██║  ██║   ██║   ██║     ██║     ">>banner
+echo "  ██║  ██║   ██║   ██║     ██║     ">>banner
+echo "  ██████╔╝   ██║   ╚██████╗╚██████╗">>banner
+echo "  ╚═════╝    ╚═╝    ╚═════╝ ╚═════╝">>banner
+echo "">>banner
+echo "This image provides a common development and runtime environment">>banner
+echo "for the Digital Twin Cities Platform.">>banner
