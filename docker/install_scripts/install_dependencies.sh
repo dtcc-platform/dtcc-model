c7e75dac3f7db000330cbc123ae7e8619533c99b
Dag Wästberg <dwastberg@gmail.com>
2022-09-15 10:33:30 +0200

    fix install scripts

--- docker/install_scripts/install_dependencies.sh
+++ docker/install_scripts/install_dependencies.sh
@@ -1,18 +1,15 @@
-#!/bin/bash
-
-apt-get update && apt-get install -y \
-    locales \
+#!/usr/bin/bash
+apt-get update && apt-get install -y locales \
     sudo \
     build-essential \
-    cmake\
+    git cmake \
     clang-format \
     clang-tidy \
-    libprotobuf-dev\
+    libprotobuf-dev \
     python3 \
     python3-pip \
     python3-dev \
     protobuf-compiler
 
-./install_python.sh
 ./install_py_libs.sh
 ./install_assimp.sh
--- docker/install_scripts/install_py_libs.sh
+++ docker/install_scripts/install_py_libs.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 python3 -m pip install \
-	laspy[lazrs] \
-	protobuf==3.20.* \
-	h5py \
-	shapely \ 
-	pybind11
+    numpy \
+    laspy[lazrs] \
+    protobuf==3.20.* \
+    h5py \
+    shapely \
+    pybind11
 
 # These libraries require all of gdal-dev to be apt installed if you want to install them via pip
 # so we install them like this for now
-apt-get update && apt-get install -y python3-fiona python3-rasterio
+apt-get install -y python3-fiona python3-rasterio
