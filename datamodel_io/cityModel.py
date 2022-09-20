8b8239f7018b63acf18d82f4a1dc30056f9471a4
Dag Wästberg <dwastberg@gmail.com>
2022-09-20 11:13:55 +0200

    format python

--- datamodel_io/cityModel.py
+++ datamodel_io/cityModel.py
@@ -7,9 +7,9 @@ import shapely.geometry
 from dtcc.dtcc_pb2 import Polygon, Building, LinearRing, Vector2D, CityModel
 
 #%%
-def cleanLinearRing(coords, tol = 0.1):
+def cleanLinearRing(coords, tol=0.1):
     s = shapely.geometry.Polygon(coords)
-    s = shapely.geometry.polygon.orient(s,1) # make ccw
+    s = shapely.geometry.polygon.orient(s, 1)  # make ccw
     s = s.simplify(tol)
     return list(s.exterior.coords)[:-1]
 
@@ -27,12 +27,13 @@ def buildLinearRing(coords):
     lr.vertices.extend(vertices)
     return lr
 
+
 def buildPolygon(geom_coords):
     polygon = Polygon()
     shell = geom_coords.pop(0)
     shell = buildLinearRing(shell)
     polygon.shell.CopyFrom(shell)
-    if len(geom_coords)> 0:
+    if len(geom_coords) > 0:
         holes = []
         for hole in geom_coords:
             hole = buildLinearRing(hole)
@@ -40,50 +41,64 @@ def buildPolygon(geom_coords):
         polygon.holes.extend(holes)
     return polygon
 
+
 def pbFootprint2Shapely(pb_footprint):
     shell = []
     holes = []
     for vert in pb_footprint.shell.vertices:
-        shell.append((vert.x,vert.y))
+        shell.append((vert.x, vert.y))
     for h in pb_footprint.holes:
         hole = []
         for vert in h.vertices:
-            hole.append((vert.x,vert.y))
+            hole.append((vert.x, vert.y))
         if len(hole) > 0:
             holes.append(hole)
-    poly = shapely.geometry.Polygon(shell, holes = holes)
+    poly = shapely.geometry.Polygon(shell, holes=holes)
     return poly
 
-def loadBuildings(filename,uuid_field = 'id', height_field = '', return_serialized=False):
+
+def loadBuildings(filename, uuid_field="id", height_field="", return_serialized=False):
     cityModel = CityModel()
     buildings = []
     has_height_field = len(height_field) == 0
     with fiona.open(filename) as src:
         for s in src:
-            geom_type = s['geometry']['type']
-            if geom_type == 'Polygon':
+            geom_type = s["geometry"]["type"]
+            if geom_type == "Polygon":
                 building = Building()
-                if uuid_field in s['properties']:
-                    building.uuid = str(s['properties'][uuid_field])
-                if has_height_field and height_field in s['properties'] and s['properties'][height_field]:
+                if uuid_field in s["properties"]:
+                    building.uuid = str(s["properties"][uuid_field])
+                if (
+                    has_height_field
+                    and height_field in s["properties"]
+                    and s["properties"][height_field]
+                ):
                     try:
-                        building.height = float(s['properties'][height_field])
+                        building.height = float(s["properties"][height_field])
                     except ValueError:
-                        print(f"Error cannot parse height field: {s['properties'][height_field]}")
-                footprint = buildPolygon(s['geometry']['coordinates'])
+                        print(
+                            f"Error cannot parse height field: {s['properties'][height_field]}"
+                        )
+                footprint = buildPolygon(s["geometry"]["coordinates"])
                 building.footPrint.CopyFrom(footprint)
                 buildings.append(building)
-            if geom_type == 'MultiPolygon':
-                for idx, polygon in enumerate(s['geometry']['coordinates']):
+            if geom_type == "MultiPolygon":
+                for idx, polygon in enumerate(s["geometry"]["coordinates"]):
                     building = Building()
-                    if uuid_field in s['properties']:
-                        uuid = str(s['properties'][uuid_field]) + f"-{idx}"
+                    if uuid_field in s["properties"]:
+                        uuid = str(s["properties"][uuid_field]) + f"-{idx}"
                         building.uuid = uuid
-                    if has_height_field and height_field in s['properties'] and s['properties'][height_field]:
+                    if (
+                        has_height_field
+                        and height_field in s["properties"]
+                        and s["properties"][height_field]
+                    ):
                         try:
-                            building.height = float(s['properties'][height_field])
+                            building.height = float(s["properties"][height_field])
                         except ValueError:
-                            print(f"Error cannot parse height field: {s['properties'][height_field]}")
+                            print(
+                                f"Error cannot parse height field: {s['properties'][height_field]}"
+                            )
                     footprint = buildPolygon(polygon)
                     building.footPrint.CopyFrom(footprint)
                     buildings.append(building)
@@ -93,38 +108,34 @@ def loadBuildings(filename,uuid_field = 'id', height_field = '', return_serializ
     else:
         return cityModel
 
-def writeCityModel(city_model, out_file, output_format = ".shp"):
-    if not output_format.startswith('.'):
-        output_format = '.' + output_format
-    if not output_format in ['.shp','.json','.geojson','.gpkg']:
+
+def writeCityModel(city_model, out_file, output_format=".shp"):
+    if not output_format.startswith("."):
+        output_format = "." + output_format
+    if not output_format in [".shp", ".json", ".geojson", ".gpkg"]:
         print(f"Error! Format {output_format} not recognized")
         return None
     driver = {
-        '.shp': 'ESRI Shapefile',
-        '.json':'GeoJSON',
-        '.geojson': 'GeoJSON',
-        '.gpkg': 'GPKG'
+        ".shp": "ESRI Shapefile",
+        ".json": "GeoJSON",
+        ".geojson": "GeoJSON",
+        ".gpkg": "GPKG",
     }
 
     schema = {
-        'geometry':'Polygon',
-        'properties':{
-            'id':'str',
-            'height':'float',
-            'ground_height': 'float'
-        }
+        "geometry": "Polygon",
+        "properties": {"id": "str", "height": "float", "ground_height": "float"},
     }
-    with fiona.open(out_file,'w',driver[output_format],schema) as dst:
+    with fiona.open(out_file, "w", driver[output_format], schema) as dst:
         for building in city_model.buildings:
             shapely_footprint = pbFootprint2Shapely(building.footPrint)
-            dst.write({
-                'geometry': shapely.geometry.mapping(shapely_footprint),
-                'properties':{
-                    'id': building.uuid,
-                    'height': building.height,
-                    'ground_height':building.groundHeight
+            dst.write(
+                {
+                    "geometry": shapely.geometry.mapping(shapely_footprint),
+                    "properties": {
+                        "id": building.uuid,
+                        "height": building.height,
+                        "ground_height": building.groundHeight,
+                    },
                 }
-            })
-        
-        
-
+            )
--- datamodel_io/pointCloud.py
+++ datamodel_io/pointCloud.py
@@ -3,7 +3,13 @@ import laspy
 from pblib.generate_protobuf import PBPointCloud
 from dtcc.dtcc_pb2 import PointCloud
 
-def loadLAS(filename, points_only = False, points_classification_only = False, return_serialized=True):
+
+def loadLAS(
+    filename,
+    points_only=False,
+    points_classification_only=False,
+    return_serialized=True,
+):
     las = laspy.read(filename)
     pts = las.xyz
     classification = np.array([]).astype(np.uint8)
@@ -12,7 +18,7 @@ def loadLAS(filename, points_only = False, points_classification_only = False, r
     numberOfReturns = np.array([]).astype(np.uint8)
     if not points_only:
         classification = np.array(las.classification)
-    if not(points_only or points_classification_only):
+    if not (points_only or points_classification_only):
         intensity = np.array(las.intensity)
         returnNumber = np.array(las.return_num)
         numberOfReturns = np.array(las.num_returns)
@@ -20,15 +26,14 @@ def loadLAS(filename, points_only = False, points_classification_only = False, r
     pb = PBPointCloud(pts, classification, intensity, returnNumber, numberOfReturns)
     if return_serialized:
         return pb
-    else: 
+    else:
         pc = PointCloud()
         pc.ParseFromString(pb)
         return pc
+
 
 def writeLAS(pointcloud, las_file):
     pass
     # WIP
     # hdr = laspy.header.Header()
     # outfile = laspy.file.File(las_file,mode='w',header=hdr)
-    
-
