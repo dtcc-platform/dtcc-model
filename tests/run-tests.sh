92d0b07dcb014935eea6cd5e299984c05bb4b594
Dag Wästberg <dwastberg@gmail.com>
2023-03-02 14:04:55 +0100

    fix test runner

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,10 +2,14 @@
 
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "Running tests from ${SCRIPT_DIR}"
 pushd ${SCRIPT_DIR}
+
+mkdir -p build
+cd build
 echo "Running C++ unit tests!"
 echo "-----------------------"
-
-./unittests_cpp/bin/run-unittests
-
-
+cmake ..
+make
+./unittests_cpp/run-unittests
+popd
