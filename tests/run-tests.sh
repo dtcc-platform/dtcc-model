ea9a006a98c66d7b4a61b46f9a0b2fe625a9cd70
Anders Logg <logg@chalmers.se>
2023-03-06 16:17:24 +0100

    Restructure unit test directory (to be continued)

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Running C++ unit tests"
+echo "======================"
+./cpp/bin/run-unittests
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
-pushd $SCRIPT_DIR
-echo "Running tests from ${SCRIPT_DIR}"
-
-mkdir -p build
-cd build
-echo "Running C++ unit tests!"
-echo "-----------------------"
-cmake ..
-make
-./unittests_cpp/run-unittests
-popd
-
-echo "Running Python unit tests!"
-echo "--------------------------"
-python3 -m unittest discover -s unittests_py -p "test_*.py"
+echo "Running Python unit tests"
+echo "========================="
+python -m unittest discover python
