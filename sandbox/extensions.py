e9362cd3b2d3bc689610b2dbb865971e636bc747
Anders Logg <logg@chalmers.se>
2023-03-29 12:56:14 +0200

    Add extensions for PointCloud

--- sandbox/extensions.py
+++ sandbox/extensions.py
@@ -4,11 +4,16 @@ from dtcc_model import *
 
 x2 = Vector3D()
 x3 = Vector3D()
+pointcloud = PointCloud()
 building = Building()
 citymodel = CityModel()
 
 x2.x = 3.0
 x2.y = 5.0
+
+pointcloud.add_point(x3)
+pointcloud.add_point(x3)
+pointcloud.add_point(x3)
 
 building.uuid = 'da39f9a4-2ae1-402b-8b00-897b11e37c05'
 building.height = 250.0
@@ -17,6 +22,7 @@ citymodel.add_building(building)
 
 print(x2)
 print(x3)
+print(pointcloud)
 print(building)
 print(citymodel)
 
