79037a629083cb23b2fdcab6667174135d065d2f
Anders Logg <logg@chalmers.se>
2023-04-05 15:05:15 +0200

    Add __all__

--- src/dtcc_model/__init__.py
+++ src/dtcc_model/__init__.py
@@ -1,5 +1,7 @@
 from dtcc_model.dtcc_pb2 import *
 
+__all__ = ['PointCloud', 'Building', 'CityModel']
+
 import dtcc_model.extensions.vector2d
 import dtcc_model.extensions.vector3d
 import dtcc_model.extensions.pointcloud
