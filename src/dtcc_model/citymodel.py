f0e16c8df9730bf951a22ad014645f46058084bf
Dag Wästberg <dwastberg@gmail.com>
2023-04-15 19:26:14 +0200

    building model

--- src/dtcc_model/citymodel.py
+++ src/dtcc_model/citymodel.py
@@ -1,15 +1,19 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import numpy as np
 from .building import Building
+from typing import Union, tuple
 
 
 @dataclass
 class CityModel:
-    buildings: list[Building]
-    terrain: np.ndarray  # Height grid or Mesh?
-    crs: str
-    origin: tuple(float, float)
-    bounds: tuple(float, float, float, float)
+    buildings: list[Building] = field(default_factory=list)
+    terrain: np.ndarray = np.empty((0, 2), dtype=np.float64)  # Height grid or Mesh?
+    crs: str = ""
+    origin: tuple([float, float]) = (0, 0)
+    bounds: tuple([float, float, float, float]) = (0, 0, 0, 0)
 
     def __str__(self):
         return f"CityModel with {len(self.buildings)} buildings"
+
+    def __len__(self):
+        return len(self.buildings)
---
+++ tests/python/test_building.py
@@ -0,0 +1,36 @@
+import unittest
+from pathlib import Path
+from dtcc_model.building import Building
+from tempfile import NamedTemporaryFile
+
+data_dir = (Path(__file__).parent / ".." / "data").resolve()
+
+
+class TestBuilding(unittest.TestCase):
+    def test_empty(self):
+        b = Building()
+        self.assertEqual(b.uuid, "")
+        self.assertEqual(b.footprint.area, 0)
+        self.assertEqual(b.height, 0)
+        self.assertEqual(b.ground_level, 0)
+        self.assertEqual(b.roofpoints.shape[1], 3)
+        self.assertEqual(b.crs, "")
+        self.assertEqual(b.error, 0)
+        self.assertEqual(b.attributes, {})
+
+    def test_from_proto(self):
+        b = Building()
+        with (data_dir / "building1.pb").open("rb") as f:
+            b.from_proto(f.read())
+        self.assertEqual(b.uuid, "0-1")
+        self.assertGreater(b.area(), 10)
+
+    def test_to_proto(self):
+        b = Building()
+        with (data_dir / "building2.pb").open("rb") as f:
+            b.from_proto(f.read())
+        # tmpfile = NamedTemporaryFile(suffix=".pb", delete=False)
+        pb_building = b.to_proto()
+        self.assertEqual(pb_building.uuid, "3")
+        self.assertEqual(len(pb_building.footPrint.shell.vertices), 5)
+        self.assertEqual(len(pb_building.footPrint.holes), 1)
