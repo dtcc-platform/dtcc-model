1c7c0690ad97301bd389bf9c3de69d4fb52d4101
Anders Logg <logg@chalmers.se>
2023-04-16 19:48:29 +0200

    Initial work on new mesh classes

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,44 +1,66 @@
 # Playground for testing
 
 from dtcc_model import *
-from dtcc_model.logging import *
 
-set_log_level(logging.DEBUG)
+from numpy import array
 
-x2 = Vector3D()
-x3 = Vector3D()
-triangle = Triangle()
-pointcloud = PointCloud()
-mesh = Mesh()
-building = Building()
-citymodel = CityModel()
+v = array([[0, 0, 0], [1, 0, 0], [0, 1, 0]], dtype=float)
+n = array([[0, 0, 1]], dtype=float)
+t = array([[0, 1, 2]])
 
-x2.x = 3.0
-x2.y = 5.0
+mesh = Mesh(v, n, t)
+proto_mesh = mesh.to_proto()
+mesh.from_proto(proto_mesh)
 
-triangle.v0 = 0
-triangle.v1 = 1
-triangle.v2 = 2
+print(mesh)
 
-pointcloud.add_point(x3)
-pointcloud.add_point(x3)
-pointcloud.add_point(x3)
 
-mesh.add_vertex(x3)
-mesh.add_vertex(x3)
-mesh.add_vertex(x3)
-mesh.add_face(triangle)
+# print(dir(mesh))
 
-building.uuid = 'da39f9a4-2ae1-402b-8b00-897b11e37c05'
-building.height = 250.0
-citymodel.add_building(building)
-citymodel.add_building(building)
+# print(mesh)
+# print(proto_mesh)
 
-info(x2)
-info(x3)
-info(pointcloud)
-info(mesh)
-info(building)
-info(citymodel)
+# from dtcc_model import *
+# from dtcc_model.logging import *
 
-citymodel.remove_building(building)
+# set_log_level(logging.DEBUG)
+
+# mesh = Mesh()
+
+# x2 = Vector3D()
+# x3 = Vector3D()
+# triangle = Triangle()
+# pointcloud = PointCloud()
+# mesh = Mesh()
+# building = Building()
+# citymodel = CityModel()
+
+# x2.x = 3.0
+# x2.y = 5.0
+
+# triangle.v0 = 0
+# triangle.v1 = 1
+# triangle.v2 = 2
+
+# pointcloud.add_point(x3)
+# pointcloud.add_point(x3)
+# pointcloud.add_point(x3)
+
+# mesh.add_vertex(x3)
+# mesh.add_vertex(x3)
+# mesh.add_vertex(x3)
+# mesh.add_face(triangle)
+
+# building.uuid = 'da39f9a4-2ae1-402b-8b00-897b11e37c05'
+# building.height = 250.0
+# citymodel.add_building(building)
+# citymodel.add_building(building)
+
+# info(x2)
+# info(x3)
+# info(pointcloud)
+# info(mesh)
+# info(building)
+# info(citymodel)
+
+# citymodel.remove_building(building)
--- src/dtcc_model/__init__.py
+++ src/dtcc_model/__init__.py
@@ -1,13 +1,9 @@
-from dtcc_model.dtcc_pb2 import *
+import dtcc_model.dtcc_pb2 as proto
 
-__all__ = ['Vector2D', 'Vector3D', 'Triangle', 'Tetrahedron',
-           'PointCloud',  'Mesh', 'VolumeMesh',
-           'Building', 'CityModel']
+from dtcc_model.mesh import Mesh
 
-import dtcc_model.extensions.vector2d
-import dtcc_model.extensions.vector3d
-import dtcc_model.extensions.pointcloud
-import dtcc_model.extensions.mesh
-import dtcc_model.extensions.volumemesh
-import dtcc_model.extensions.building
-import dtcc_model.extensions.citymodel
+__all__ = ['Mesh', 'proto']
+
+# __all__ = ['Vector2D', 'Vector3D', 'Triangle', 'Tetrahedron',
+#           'PointCloud',  'Mesh', 'VolumeMesh',
+#           'Building', 'CityModel']
