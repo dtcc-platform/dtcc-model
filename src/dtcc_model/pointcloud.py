b19cacddff14198b38fe17ec4bf0cf38f2f15456
Dag Wästberg <dwastberg@gmail.com>
2023-04-16 12:36:45 +0200

    new pointcloud model

--- src/dtcc_model/pointcloud.py
+++ src/dtcc_model/pointcloud.py
@@ -24,8 +24,48 @@ class Pointcloud:
     def used_classifications(self) -> set:
         return set(np.unique(self.classification))
 
+    def calc_bounds(self):
+        self.bounds = (
+            self.points[:, 0].min(),
+            self.points[:, 1].min(),
+            self.points[:, 0].max(),
+            self.points[:, 1].max(),
+        )
+
     def from_proto(self, proto_pointcloud: Union[proto.PointCloud, bytes]):
-        pass
+        if isinstance(proto_pointcloud, bytes):
+            _pointcloud = proto.PointCloud()
+            _pointcloud.ParseFromString(proto_pointcloud)
+            proto_pointcloud = _pointcloud
+        self.points = np.array(proto_pointcloud.points).reshape(-1, 3)
+        self.classification = np.array(proto_pointcloud.classification).astype(np.uint8)
+        self.intensity = np.array(proto_pointcloud.intensity).astype(np.uint16)
+        self.return_number = np.array(proto_pointcloud.returnNumber).astype(np.uint8)
+        self.number_of_returns = np.array(proto_pointcloud.numReturns).astype(np.uint8)
+        self.bounds = (
+            proto_pointcloud.bounds.p.x,
+            proto_pointcloud.bounds.p.y,
+            proto_pointcloud.bounds.q.x,
+            proto_pointcloud.bounds.q.y,
+        )
+        self.origin = (
+            proto_pointcloud.georeference.x0,
+            proto_pointcloud.georeference.y0,
+        )
+        self.crs = proto_pointcloud.georeference.crs
 
     def to_proto(self) -> proto.PointCloud:
-        pass
+        proto_pc = proto.PointCloud()
+        proto_pc.points.extend(self.points.reshape(-1).tolist())
+        proto_pc.classification.extend(self.classification.reshape(-1).tolist())
+        proto_pc.intensity.extend(self.intensity.reshape(-1).tolist())
+        proto_pc.returnNumber.extend(self.return_number.reshape(-1).tolist())
+        proto_pc.numReturns.extend(self.number_of_returns.reshape(-1).tolist())
+        proto_pc.bounds.p.x = self.bounds[0]
+        proto_pc.bounds.p.y = self.bounds[1]
+        proto_pc.bounds.q.x = self.bounds[2]
+        proto_pc.bounds.q.y = self.bounds[3]
+        proto_pc.georeference.x0 = self.origin[0]
+        proto_pc.georeference.y0 = self.origin[1]
+        proto_pc.georeference.crs = self.crs
+        return proto_pc
--- tests/python/test_pointcloud.py
+++ tests/python/test_pointcloud.py
@@ -1,5 +1,5 @@
 import unittest
-
+import numpy as np
 from dtcc_model.pointcloud import Pointcloud
 
 
@@ -11,8 +11,41 @@ class TestPointcloud(unittest.TestCase):
         self.assertEqual(pc.origin, (0, 0))
         self.assertEqual(pc.bounds, (0, 0, 0, 0))
 
-    def test_from_proto(self):
-        pass
+    def test_calc_bounds(self):
+        pc = Pointcloud()
+        pc.points = np.array([[0, 0, 0], [1, 1, 1], [2, 2, 2]])
+        pc.calc_bounds()
+        self.assertEqual(pc.bounds, (0, 0, 2, 2))
 
     def test_to_proto(self):
-        pass
+        pc = Pointcloud()
+        pc.points = np.array([[0, 0, 0], [1, 1, 1], [2, 2, 2]])
+        pc.calc_bounds()
+        pc.crs = "EPSG:3857"
+        pc.origin = (0, 0)
+        pc.classification = np.array([1, 2, 3])
+        pc.intensity = np.array([1, 2, 3])
+        pc.return_number = np.array([1, 1, 1])
+        pc.number_of_returns = np.array([1, 1, 1])
+
+        proto_pc = pc.to_proto()
+        self.assertEqual(proto_pc.points, [0, 0, 0, 1, 1, 1, 2, 2, 2])
+        self.assertEqual(proto_pc.classification, [1, 2, 3])
+
+    def test_from_proto(self):
+        pc = Pointcloud()
+        pc.points = np.array([[0, 0, 0], [1, 1, 1], [2, 2, 2]])
+        pc.calc_bounds()
+        pc.crs = "EPSG:3857"
+        pc.origin = (0, 0)
+        pc.classification = np.array([1, 2, 3])
+        pc.intensity = np.array([1, 2, 3])
+        pc.return_number = np.array([1, 1, 1])
+        pc.number_of_returns = np.array([1, 1, 1])
+
+        proto_pc = pc.to_proto()
+
+        pc2 = Pointcloud()
+        pc2.from_proto(proto_pc)
+        self.assertEqual(pc2.points.tolist(), pc.points.tolist())
+        self.assertEqual(pc2.classification.tolist(), pc.classification.tolist())
