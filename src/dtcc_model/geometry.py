384bc882d1ed4267bbf5b55d2fd8dce4b7007e67
Anders Logg <logg@chalmers.se>
2023-08-18 11:34:22 +0200

    Update docstrings for geometry.py using latest prompt

--- src/dtcc_model/geometry.py
+++ src/dtcc_model/geometry.py
@@ -12,19 +12,18 @@ from . import dtcc_pb2 as proto
 
 @dataclass
 class Bounds(DTCCModel):
-    """
-    A class representing spatial bounds.
+    """Represents the boundaries of a rectangular region.
 
     Attributes
     ----------
     xmin : float
-        The minimum x-coordinate.
+        Minimum x-coordinate.
     ymin : float
-        The minimum y-coordinate.
+        Minimum y-coordinate.
     xmax : float
-        The maximum x-coordinate.
+        Maximum x-coordinate.
     ymax : float
-        The maximum y-coordinate.
+        Maximum y-coordinate.
     """
 
     xmin: float = 0.0
@@ -33,116 +32,165 @@ class Bounds(DTCCModel):
     ymax: float = 0.0
 
     def __str__(self):
-        """
-        Returns a string representation of the Bounds instance.
-
-        Returns
-        -------
-        str
-            A string detailing the bounds.
-        """
+        """Returns a formatted string representation of the bounds."""
         return f"DTCC Bounds {self.bndstr}"
 
     @property
     def bndstr(self) -> str:
-        """Returns the bounds as a string in format: [xmin, xmax] x [ymin, ymax]."""
+        """Returns the bounds as a formatted string.
+
+        Returns
+        -------
+        str
+            Formatted string of bounds.
+        """
         return f"[{self.xmin}, {self.xmax}] x [{self.ymin}, {self.ymax}]"
 
     @property
     def width(self) -> float:
-        """Returns the width of the bounds."""
+        """Returns the width of the bounds.
+
+        Returns
+        -------
+        float
+            Width of the bounds.
+        """
         return self.xmax - self.xmin
 
     @property
     def height(self) -> float:
-        """Returns the height of the bounds."""
+        """Returns the height of the bounds.
+
+        Returns
+        -------
+        float
+            Height of the bounds.
+        """
         return self.ymax - self.ymin
 
     @property
     def north(self) -> float:
-        """Returns the northernmost coordinate (ymax)."""
+        """Returns the northernmost coordinate.
+
+        Returns
+        -------
+        float
+            Northernmost y-coordinate.
+        """
         return self.ymax
 
     @property
     def south(self) -> float:
-        """Returns the southernmost coordinate (ymin)."""
+        """Returns the southernmost coordinate.
+
+        Returns
+        -------
+        float
+            Southernmost y-coordinate.
+        """
         return self.ymin
 
     @property
     def east(self) -> float:
-        """Returns the easternmost coordinate (xmax)."""
+        """Returns the easternmost coordinate.
+
+        Returns
+        -------
+        float
+            Easternmost x-coordinate.
+        """
         return self.xmax
 
     @property
     def west(self) -> float:
-        """Returns the westernmost coordinate (xmin)."""
+        """Returns the westernmost coordinate.
+
+        Returns
+        -------
+        float
+            Westernmost x-coordinate.
+        """
         return self.xmin
 
     @property
     def tuple(self) -> tuple:
-        """Returns the bounds as a tuple (xmin, ymin, xmax, ymax)."""
+       """Returns the bounds as a tuple.
+
+        Returns
+        -------
+        tuple
+            Tuple representation of bounds.
+        """        
         return (self.xmin, self.ymin, self.xmax, self.ymax)
 
     @property
     def area(self) -> float:
-        """Returns the area covered by the bounds."""
+        """Returns the area enclosed by the bounds.
+
+        Returns
+        -------
+        float
+            Area of the bounds.
+        """        
         return self.width * self.height
 
     def center(self) -> tuple:
-        """Returns the center point of the bounds."""
+        """Returns the center point of the bounds.
+
+        Returns
+        -------
+        tuple
+            Center point as (x, y).
+        """        
         return (self.xmin + self.width / 2, self.ymin + self.height / 2)
 
     def buffer(self, distance: float):
-        """
-        Expands the bounds by the given distance in all directions.
+        """Increases the size of the bounds by a specified distance.
 
         Parameters
         ----------
         distance : float
             The distance to expand the bounds by.
-        """
+        """        
         self.xmin -= distance
         self.ymin -= distance
         self.xmax += distance
         self.ymax += distance
 
     def union(self, other):
-        """
-        Modifies the bounds to be the union of itself and another Bounds object.
+        """Merges this bounds with another, taking the outermost bounds.
 
         Parameters
         ----------
         other : Bounds
-            The other bounds to union with.
-        """
+            The other bounds to merge with.
+        """        
         self.xmin = min(self.xmin, other.xmin)
         self.ymin = min(self.ymin, other.ymin)
         self.xmax = max(self.xmax, other.xmax)
         self.ymax = max(self.ymax, other.ymax)
 
     def intersect(self, other):
-        """
-        Modifies the bounds to be the intersection of itself and another Bounds object.
+        """Modifies this bounds to be the intersection with another.
 
         Parameters
         ----------
         other : Bounds
             The other bounds to intersect with.
-        """
+        """        
         self.xmin = max(self.xmin, other.xmin)
         self.ymin = max(self.ymin, other.ymin)
         self.xmax = min(self.xmax, other.xmax)
         self.ymax = min(self.ymax, other.ymax)
 
     def from_proto(self, pb: Union[proto.Bounds, bytes]):
-        """
-        Sets the bounds from a protobuf message or bytes.
+        """Loads the bounds from a protobuf representation.
 
         Parameters
         ----------
         pb : Union[proto.Bounds, bytes]
-            The protobuf message or its byte representation.
-        """
+            The protobuf representation or bytes.
+        """        
         if isinstance(pb, bytes):
             pb = proto.Bounds.FromString(pb)
         self.xmin = pb.xmin
@@ -151,6 +199,13 @@ class Bounds(DTCCModel):
         self.ymax = pb.ymax
 
     def to_proto(self) -> proto.Bounds:
+        """Converts the bounds to a protobuf representation.
+
+        Returns
+        -------
+        proto.Bounds
+            Protobuf representation of the bounds.
+        """        
         pb = proto.Bounds()
         pb.xmin = self.xmin
         pb.xmax = self.xmax
@@ -161,20 +216,20 @@ class Bounds(DTCCModel):
 
 @dataclass
 class Georef(DTCCModel):
-    """
-    A class representing geospatial reference information.
-
+    """Represents georeferencing information for spatial data.
+    
     Attributes
     ----------
     crs : str
-        The coordinate reference system identifier (e.g., 'EPSG:4326').
+        Coordinate reference system string.
     epsg : int
-        The EPSG code corresponding to the coordinate reference system.
+        EPSG code corresponding to the coordinate reference system.
     x0 : float
-        The x-coordinate of the reference origin.
+        x-coordinate of the origin.
     y0 : float
-        The y-coordinate of the reference origin.
+        y-coordinate of the origin.    
     """
+
 
     crs: str = ""
     epsg: int = 0
@@ -182,27 +237,19 @@ class Georef(DTCCModel):
     y0: float = 0.0
 
     def __str__(self):
-        """
-        Returns a string representation of the Georef instance.
-
-        Returns
-        -------
-        str
-            A string detailing the geospatial reference information.
-        """
+        """Returns a formatted string representation of the georeferencing information."""
         return (
             f"DTCC Georef {self.crs} ({self.epsg}) with origin ({self.x0}, {self.y0})"
         )
 
     def from_proto(self, pb: Union[proto.Georef, bytes]):
-        """
-        Sets the geospatial reference information from a protobuf message or bytes.
+        """Loads the georeferencing information from a protobuf representation.
 
         Parameters
         ----------
         pb : Union[proto.Georef, bytes]
-            The protobuf message or its byte representation.
-        """
+            The protobuf representation or bytes.
+        """        
         if isinstance(pb, bytes):
             pb = proto.Georef.FromString(pb)
         self.crs = pb.crs
@@ -211,14 +258,13 @@ class Georef(DTCCModel):
         self.y0 = pb.y0
 
     def to_proto(self) -> proto.Georef:
-        """
-        Converts the geospatial reference information to a protobuf message.
+        """Converts the georeferencing information to a protobuf representation.
 
         Returns
         -------
         proto.Georef
-            The protobuf representation of the geospatial reference information.
-        """
+            Protobuf representation of the georeferencing information.
+        """        
         pb = proto.Georef()
         pb.crs = self.crs
         pb.epsg = self.epsg
