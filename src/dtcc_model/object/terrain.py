3c1b4d914fe9d5bc2c892908f510e6171346eee9
Dag Wästberg <dwastberg@gmail.com>
2024-01-29 10:52:03 +0100

    add terrain object

---
+++ src/dtcc_model/object/terrain.py
@@ -0,0 +1,18 @@
+# Copyright(C) 2024 Dag Wästberg
+# Licensed under the MIT License
+
+from dataclasses import dataclass
+
+from .object import Object
+
+@dataclass
+class Terrain(Object):
+    """Represents a terrain object in a city."""
+
+
+#TODO: Implement to_proto and from_proto
+    def to_proto(self):
+        pass
+
+    def from_proto(self, pb):
+        pass
