cd048dd1eeeea61d17d53bde1c69eef5624ac262
Dag Wästberg <dwastberg@gmail.com>
2024-03-13 10:43:20 +0100

    update polygon

--- src/dtcc_model/geometry/polygon.py
+++ src/dtcc_model/geometry/polygon.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 from .bounds import Bounds
 from .geometry import Geometry
 from shapely.geometry import Polygon as ShapelyPolygon
+import numpy as np
 
 
 @dataclass
@@ -13,3 +14,19 @@ class Polygon(Geometry):
 
     def from_proto(self, pb):
         return None
+
+    @property
+    def shapely(self):
+        return self.geom
+
+    @property
+    def vertices(self):
+        return np.array(self.geom.exterior.coords)
+
+    @property
+    def holes(self):
+        return [np.array(hole.coords) for hole in self.geom.interiors]
+
+    @property
+    def area(self):
+        return self.geom.area
